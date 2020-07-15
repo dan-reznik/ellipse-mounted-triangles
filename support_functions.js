d5d8b2f71da4ee899be5101c79fc78bdaa55ed3a
Dan Reznik <dreznik@gmail.com>
2020-07-14 21:32:51 -0300

    parser

---
+++ support_functions.js
@@ -0,0 +1,42 @@
+// triAreaHeron, lawOfCosines, getSin, oneOv
+// cosDoubleAngle, sinHalfAngle, cosHalfAngle,
+// sinCosTripleAngle, getSinApmB
+
+let cPi3 = Math.cos(Math.PI/3);
+let sPi3 = Math.sin(Math.PI/3);
+let cPi6 = Math.cos(Math.PI/6);
+let sPi6 = Math.sin(Math.PI/6);
+
+function oneOv(x) { return 1.0/x; }
+
+function triAreaHeron(a, b, c) {
+  let s = (a + b + c)/2.0;
+  return sqrt(s*(s-a)*(s-b)*(s-c));
+}
+
+function lawOfCosines(a, b, c) { return (b*b+c*c-a*a)/(2.0*b*c); }
+
+function getSin(theCos) { return sqrt(1.0 - theCos*theCos); }
+
+function cosDoubleAngle(cosAngle) { return 2*cosAngle*cosAngle - 1.0; }
+function cosHalfAngle(cosAngle) { return sqrt((1.0+cosAngle)/2); }
+function sinHalfAngle(cosAngle) { return sqrt((1.0-cosAngle)/2); }
+
+function getSinApmB(sa, sb, ca, cb) { return [sa*cb + sb*ca, sa*cb - sb*ca]; }
+
+function getSinApmB1(sa, sb, ca, cb) { return sa*cb + sb*ca; }
+function getSinApmB2(sa, sb, ca, cb) { return sa*cb - sb*ca; }
+
+function sinCosTripleAngle(s, c, s2, c2) {
+  let c3 = c2*c - s2*s;
+  let s3 = s2*c + s*c2;
+  return [s3, c3];
+}
+
+function cosTripleAngle(s, c, s2, c2) {
+  return c2*c - s2*s;
+}
+
+function sinTripleAngle(s, c, s2, c2) {
+  return s2*c + s*c2;
+}
